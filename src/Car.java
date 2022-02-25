8c04ab4e75d882aa2c131df6e78427144fb6ab20
shofiuddin <md.shofiuddin@bjitgroup.com>
2022-02-26 01:34:52 +0600

    [Commit-1] Simple Car Image move on button pressed - new Branch SD1-Car-Drive-Game is created - Background Image added - Car Image added - Car move on button press

--- src/Car.java
+++ src/Car.java
@@ -3,17 +3,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Car {
-    Image carImage;
-    int x;
-    Car(){
-        ImageIcon imageIcon = new ImageIcon("car.png");
-        carImage = imageIcon.getImage();
-        x = 300;
-
-    }
-     public void whenkeypressed(KeyEvent e){
-        int key = e.getKeyCode();
-        if(key== 39) x = x+2;
-        if(key== 37) x= x-2;
-    }
+    ImageIcon imageIcon = new ImageIcon("car.png");
+    Image carImage = imageIcon.getImage();
+    int x = 300;
 }
