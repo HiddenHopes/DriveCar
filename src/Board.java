bdd022e00937f23a6ad2811197e2424b485d248c
shofiuddin <md.shofiuddin@bjitgroup.com>
2022-02-26 01:34:52 +0600

    [Commit-1] Simple Car Image move on button pressed - new Branch SD1-Car-Drive-Game is created - Background Image added - Car Image added - Car move on button press

--- src/Board.java
+++ src/Board.java
@@ -1,40 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 
-public class Board extends JPanel implements ActionListener {
-    Image carbg;
+public class Board extends JPanel {
+    Image backgroundImage;
     Car car;
-    Timer time;
 
-    Board(){
-        ImageIcon imageIcon = new ImageIcon("carbg.png");
-        carbg = imageIcon.getImage();
+    Board() {
+        ImageIcon imageIcon = new ImageIcon("background.png");
+        backgroundImage = imageIcon.getImage();
         car = new Car();
-        addKeyListener(new AL());
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if(e.getKeyCode()==39) car.x = car.x + 5;
+                if(e.getKeyCode()==37) car.x = car.x - 5;
+            }
+        });
         setFocusable(true);
-        time = new Timer(5,this);
-        time.start();
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawImage(carbg, 0,0, this);
-        g.drawImage(car.carImage, car.x,450, this );
-    }
-
-    public void actionPerformed(ActionEvent e) {
+        g.drawImage(backgroundImage, 0, 0, this);
+        g.drawImage(car.carImage, car.x, 450, this);
         repaint();
     }
-   private class AL extends KeyAdapter {
-        public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
-            car.whenkeypressed(e);
-        }
-    }
-
 }
